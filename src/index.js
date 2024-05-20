10e53cc3b172272ea9c68f42b524f4f2459fd9a6
Satavisa Das <satavisa1234@gmail.com>
2024-05-20 14:16:59 +0530

    Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const app=express();
 const PORT=process.env.PORT ||3000;
+
+app.get("/",(request,response)=>{response.send({msg:"Hello!"})});
+
 app.listen(PORT,()=>{
   console.log(`Running on Port ${PORT}`);
 });
