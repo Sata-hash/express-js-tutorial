7d3de5babacfbc4fe0fedb70ab2e5107c7e95e2e
Satavisa Das <satavisa1234@gmail.com>
2024-05-20 14:35:33 +0530

    Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const PORT=process.env.PORT ||3000;
 
 app.get("/",(request,response)=>{response.send({msg:"Hello!"})});
 
+app.get("/api/users",(request,response)=>{response.send([{id:1,username:"anson",displayname:"Anson"},
+{id:2 ,username:"satavisa",displayname:"Satavisa"},
+{id:3,username:"dia",displayname:"Dia"},
+{id:4,username:"riya",displayname:"Riya"}])});
+
+app.get("/api/products",(request,response)=>{response.send([{id:1,dish:"chicken breast",price:12.99},
+{id:2,dish:"chicken tandoori",price:13.99}])})
+
 app.listen(PORT,()=>{
   console.log(`Running on Port ${PORT}`);
 });
