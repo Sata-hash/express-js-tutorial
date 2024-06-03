d9e0d5f011d3569aca79ff8ed846a68bdc1c7f18
Satavisa Das <satavisa1234@gmail.com>
2024-06-03 08:24:27 +0530

    Learn

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ const app=express();
 const PORT=process.env.PORT ||3000;
 
 app.get("/",(request,response)=>{response.send({msg:"Hello!"})});
-
-app.get("/api/users",(request,response)=>{response.send([{id:1,username:"anson",displayname:"Anson"},
+const mockUsers=[{id:1,username:"anson",displayname:"Anson"},
 {id:2 ,username:"satavisa",displayname:"Satavisa"},
 {id:3,username:"dia",displayname:"Dia"},
-{id:4,username:"riya",displayname:"Riya"}])});
+{id:4,username:"riya",displayname:"Riya"}];
+app.get("/api/users",(request,response)=>{response.send(mockUsers)});
 
+app.get("/api/users/:id",(request,response)=>{
+  console.log(request.params);
+  const parsedId=parseInt(request.params.id);
+  console.log(parsedId);
+  if(isNaN(parsedId))
+    return response.status(400).send({msg:"Bad Request.Invalid ID"});
+  const findUser=mockUsers.find((user)=>user.id===parsedId);
+  if(!findUser)
+      return response.sendStatus(404);
+    return response.send(findUser);
+})
 app.get("/api/products",(request,response)=>{response.send([{id:1,dish:"chicken breast",price:12.99},
 {id:2,dish:"chicken tandoori",price:13.99}])})
 
